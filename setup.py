bb91d1dd8488d78b5359d8581567f1c9d3605c06
Javier Diaz <javier.diazmontes@gmail.com>
2012-09-19 11:17:38 -0400

    version file fixed

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
-"""FutureGrid: Creating a SLURM virtual Cluster via euca2tools
-
-This project creates a virtual cluster with the help of euca
-tools. The cluster uses SLURM as resource manager. A simple MPI
-program is included to test the functionality. of teh virtual cluster.
-"""
-
+from distribute_setup import use_setuptools
+use_setuptools(version="0.6.15")
 from setuptools import setup, find_packages
-import sys, os
+import sys
+sys.path.insert(0, './src')
+from teefaa import RELEASE
 
-version = '0.1'
-
-# due to a bug we are not including VERION.py yet
-# execfile('VERSION.py)
 
 classifiers = """\
 Intended Audience :: Developers
@@ -31,23 +24,13 @@ Topic :: System :: Clustering
 Topic :: System :: Distributed Computing
 """
 
-if sys.version_info < (2, 7):
-    _setup = setup
-    def setup(**kwargs):
-        if kwargs.has_key("classifiers"):
-            del kwargs["classifiers"]
-        _setup(**kwargs)
-
-doclines = __doc__.split("\n")
-
 
 setup(
     name='teefaa',
-    version=version,
-    description = doclines[0],
+    version=RELEASE,
+    description = "Futuregrid Teefaa is a set of tools that enables simplified provisioning from repository or directly from running virtual/baremetal machines.",
     classifiers = filter(None, classifiers.split("\n")),
-    long_description = "\n".join(doclines[2:]),
-    keywords='FutureGrid teefa',
+    keywords='FutureGrid, Teefaa',
     maintainer='Koji Tanaka, Javier Diaz, Gregor von Laszewski',
     maintainer_email="kj.tanaka@gmail.com",
     author='Koji Tanaka',
@@ -55,19 +38,15 @@ setup(
     url='https://github.com/futuregrid/teefaa',
     license='Apache 2.0',
     package_dir = {'': 'src'},
-    packages = find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    packages = find_packages('src'),
     
     #include_package_data=True,
-    #zip_safe=True,
-    #install_requires=[
-    #    # -*- Extra requirements: -*-
-    #],
+    zip_safe=False,
 
-    
-    #entry_points={
-    #    'console_scripts': [
-    #            'fg-teefa = futuregrid.move.teefaa.FGteefa:main',
-    #         ]},
+    entry_points={
+        'console_scripts': [
+                'fg-teefaa = teefaa.teefaa:main',
+             ]},
 
     install_requires = [
              'setuptools',
