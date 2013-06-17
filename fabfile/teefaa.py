161de06899baa6d8070314bee17e2a88125abb11
Koji Tanaka <kj.tanaka@gmail.com>
2013-06-17 14:28:41 -0400

    update

--- fabfile/teefaa.py
+++ fabfile/teefaa.py
@@ -18,7 +18,10 @@ from cuisine import *
 def bootstrap(imagename):
     ''':imagename=XXXXX | Bootstrap OS'''
 
-    env.user = 'root'
+    if not env.user == 'root':
+        print 'You need to login as root for bootstrap.'
+        print 'So add the option \"--user root\"'
+        exit(1)
 
     hostsfile = 'ymlfile/teefaa/hosts.yml'
     hosts = read_ymlfile(hostsfile)
